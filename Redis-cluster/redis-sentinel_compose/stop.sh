b20cbcc848dd1a22b9fa3ca1a65d5e33e9c08c99
bkhk06 <bkhk06@sina.com>
2019-07-02 22:55:31 +0800

    up

--- Redis-cluster/redis-sentinel_compose/stop.sh
+++
@@ -1,7 +0,0 @@
-docker stop redis-cluster_master
-docker stop redis-cluster_slave_1
-docker stop redis-cluster_slave_2
-docker stop redis-cluster_sentinel_1
-docker stop redis-cluster_sentinel_2
-docker stop redis-cluster_sentinel_3
-docker rm $(docker ps -a|grep redis-cluster)
